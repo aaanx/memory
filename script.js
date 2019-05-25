b4466aa3c8c5762cd859ea2f0ff55986c0fa32f9
Ewa <ewsekowska@gmail.com>
2019-05-25 12:29:49 +0200

    Add images to boxes

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
+var elementsNumber = 16;
+var wrapper = document.querySelector('.wrapper');
+var element = document.createElement('div');
+
+// Tworzenie pojedynczego diva
 function createElement() {
-    var wrapper = document.querySelector('.wrapper');
-    var element = document.createElement('div');
+    element = document.createElement('div');
     element.classList.add('box');
     wrapper.appendChild(element);
 }
 
+//dodanie x kart memory
 function addElements() {
-    var elementsNumber = 16;
     for (let i = 0; i < elementsNumber; i++) {
         createElement();
     }
 }
+
 addElements();
+
+// Dodawanie zdjęć
+
+function displayImages() {
+    var images = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16'];
+    var allDivs = document.querySelectorAll('div.box');
+
+    for (let i = 0; i < images.length; i++) {
+        var img = document.createElement('img');
+        img.setAttribute('src', `/img/${images[i]}.jpg`);
+        allDivs[i].appendChild(img);
+    }
+}
+displayImages()
+
+
